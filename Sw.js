6621d588e17ed2341f5f6ee5e34ac6a8477bc8d0
Mike <mike123rpg@gmail.com>
2026-02-15 14:45:29 +0100

    Add service worker for caching and fetch handling

---
+++ Sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'eggplant-collector-v1';
+const urlsToCache = [
+  './eggplant-game.html',
+  './manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
